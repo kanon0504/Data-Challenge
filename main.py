07c77a3b7ce38da4a19bf3c6c62984c21ffd07e6
kanon0504 <chaojun.maths@gmail.com>
2016-02-23 22:24:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,49 +3,159 @@ import h5py
 import matplotlib.pyplot as plt
 
 f = h5py.File('train_data.mat','r')
-refs = f['#refs#']
+ff = h5py.File('test_data.mat','r')
 data = f['data']
-cell = f[data[28][0]]
+data1 = ff['data']
 
 class cell(object):
-	def __init__(self,cell):
-		self.tgt = cell['tgt'][...][0][0]
-		# 1 tgt
-		self.id = [cell['id'][...][i][0] for i in range(len(cell['id'][...]))]
-		# 2 id
-		self.f_eeg = cell['fsample/eeg'][...][0][0]
-		self.f_wav = cell['fsample/wav'][...][0][0]
-		# 3 fsample
-		self.eeg = f[cell['eeg'][...][0][0]][...]
-		# 4 eeg
-		self.wav = f[cell['wav'][...][0][0]][...]
-		# 5 wav
-		self.sample = cell['event/eeg/sample'][...][0]
-		length = len(cell['event/eeg/value'][...][0])
-		self.value = []
-		for i in range(length):
-			if all(f[cell['event/eeg/value'][...][0][i]][...]) == all([0,0]):
-				self.value.append(0.)
-			else:
-				self.value.append(f[cell['event/eeg/value'][...][0][i]][...][0][0])
-		# 6 event
-	def var(self):
-		debut = a.sample[a.value.index(200.)]
-		fin = a.sample[a.value.index(150.0)]
-		var = []
-		for i in range(len(a.eeg)):
-			it = []
-			it.append(np.var(a.eeg[i][:debut]))
-			it.append(np.var(a.eeg[i][debut:fin]))
-			it.append(np.var(a.eeg[i][fin:]))
-			var.append(it)
-		return var
+    def __init__(self,cell):
+        self.tgt = cell['tgt'][...][0][0]
+        # 1 tgt
+        self.id = [cell['id'][...][i][0] for i in range(len(cell['id'][...]))]
+        # 2 id
+        self.f_eeg = cell['fsample/eeg'][...][0][0]
+        self.f_wav = cell['fsample/wav'][...][0][0]
+        # 3 fsample
+        self.eeg = f[cell['eeg'][...][0][0]][...]
+        # 4 eeg
+        self.wav = f[cell['wav'][...][0][0]][...]
+        # 5 wav
+        self.sample = cell['event/eeg/sample'][...][0]
+        length = len(cell['event/eeg/value'][...][0])
+        self.value = []
+        for i in range(length):
+            if all(f[cell['event/eeg/value'][...][0][i]][...]) == all([0,0]):
+                self.value.append(0.)
+            else:
+                self.value.append(f[cell['event/eeg/value'][...][0][i]][...][0][0])
+        # 6 event
+    def var(self):
+        debut = self.sample[self.value.index(200.)]
+        fin = self.sample[self.value.index(150.0)]
+        var = []
+        for i in range(len(self.eeg)):
+            it = []
+            it.append(np.var(self.eeg[i][:debut]))
+            it.append(np.var(self.eeg[i][debut:fin]))
+            it.append(np.var(self.eeg[i][fin:]))
+            var.append(it)
+        return var
 
-a = cell(f[data[57][0]])
-var = a.var()
-print np.shape(var)
+class cell1(object):
+    def __init__(self,cell):
+        self.id = [cell['id'][...][i][0] for i in range(len(cell['id'][...]))]
+        # 2 id
+        self.eeg = ff[cell['eeg'][...][0][0]][...]
+        # 4 eeg
+        self.wav = ff[cell['wav'][...][0][0]][...]
+        # 5 wav
+
+cell = [cell(f[data[i][0]]) for i in range(len(data))]
+cell1 = [cell1(ff[data1[i][0]]) for i in range(len(data1))]
+
+##################################################### updated 23 Feb ###########################################################
+def chunks(l,n):
+    for i in xrange(0,len(l),n):
+        yield l[i:i+n]
+# Devide list l into n chunks
+
+frequency = 44100
+cut = 0.05
+speech0 = []
+speech1 = []
+for i in cell:
+    duration = len(i.wav[0])/frequency
+    num_piece = duration/cut
+    i_cut = chunks(i.wav[0],int(len(i.wav[0])/num_piece))
+    bi = []
+    for j in i_cut:
+        if np.var(j) >= 0.0001:
+            bi.append(1)
+        else:
+            bi.append(0)
+    timing = []
+    it = [0]
+    counter = 1
+    for j in range(len(bi)-1):
+        if bi[j] != bi[j+1] and counter < 2:
+            it.append(j+1)
+            counter += 1
+        if bi[j] != bi[j+1] and counter == 2:
+            timing.append((it[0]*cut*frequency,it[1]*cut*frequency))
+            it = []
+            counter = 0
+    speech0.append(timing)
+
+for i in cell:
+    duration = len(i.wav[0])/frequency
+    num_piece = duration/cut
+    i_cut = chunks(i.wav[1],int(len(i.wav[1])/num_piece))
+    bi = []
+    for j in i_cut:
+        if np.var(j) >= 0.0001:
+            bi.append(1)
+        else:
+            bi.append(0)
+    timing = []
+    it = [0]
+    counter = 1
+    for j in range(len(bi)-1):
+        if bi[j] != bi[j+1] and counter < 2:
+            it.append(j+1)
+            counter += 1
+        if bi[j] != bi[j+1] and counter == 2:
+            timing.append((it[0]*cut*frequency,it[1]*cut*frequency))
+            it = []
+            counter = 0
+    speech1.append(timing)
+
+# each element in speech0/1 is a list of tuples with the
+# beginning and ending index of a sentence (0:left, 1:right)
+
+##################################################### updated 23 Feb ###########################################################
+
+
+var = [cell[i].var() for i in range(len(data))]
+# Create a matrix which stores in each line the variance of each eeg
+cell_var = []
 for i in var:
-	print i
-##############################################################################
+    temp = []
+    for j in i:
+        temp.append(max(abs(j[1]-j[0]),abs(j[1]-j[2])))
+    cell_var.append(temp)
+selected_egg = [[i for i in range(len(j)) if j[i]> 50] for j in cell_var]
+# Create a matrix which stores in each line the index whose value in its varicance(centralized) is greater than 50
 
 
+merge_eeg_0 = []
+merge_eeg_1 = []
+for i in range(len(selected_egg)):
+    if cell[i].tgt == 0:
+        merge_eeg_0 += selected_egg[i]
+    else:
+        merge_eeg_1 += selected_egg[i]
+count_eeg_0 = {}
+count_eeg_1 = {}
+for i in set(merge_eeg_0):
+    count_eeg_0[i] = merge_eeg_0.count(i)
+for i in set(merge_eeg_1):
+    count_eeg_1[i] = merge_eeg_1.count(i)
+# Count_eeg counts the occurance of all the eegs who is considered to be relevant to the ongoing of experiment
+
+print count_eeg_0
+print count_eeg_1
+c0 = []
+c1 = []
+for i in range(128):
+    if i in count_eeg_0:
+        c0.append(count_eeg_0[i])
+    else:
+        c0.append(0)
+for i in range(128):
+    if i in count_eeg_1:
+        c1.append(count_eeg_1[i])
+    else:
+        c1.append(0)
+plt.plot(c0,'g')
+plt.plot(c1,'r')
+plt.show()
